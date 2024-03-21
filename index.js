c4916ac02b8481a8cfc1d333fc3aefb98ac79cf2
JesusEmilianoRojasC <65838250+JesusEmilianoRojasC@users.noreply.github.com>
2024-03-21 10:27:10 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("Bienvenido a Nothing México");
+var negativo = "https://google.com/"
+
+
+if (window.confirm("¿Acepta los terminos y condiciones de Nothing México?") == false) {
+    cerrar = window.close("index.html");
+    window.open(negativo) + cerrar;
+  }
+
+
+let DerechosAutor = "Nothing México  /  Todos los derechos son reservados. 2024";
+
+console.log(DerechosAutor);
